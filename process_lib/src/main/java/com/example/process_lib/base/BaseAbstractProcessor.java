9dbad8d64cf33d610b9b951303012fbe02cab2ca
zhengcaifu <344984602@qq.com>
2021-03-27 10:49:26 +0400

    从android项目传慈禧编辑期的参数。

--- process_lib/src/main/java/com/example/process_lib/base/BaseAbstractProcessor.java
+++ process_lib/src/main/java/com/example/process_lib/base/BaseAbstractProcessor.java
@@ -1,18 +1,31 @@
 package com.example.process_lib.base;
 
+import java.util.Map;
+import java.util.Set;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.util.Elements;
+import javax.tools.Diagnostic;
 
 public abstract class BaseAbstractProcessor extends AbstractProcessor {
 
     protected Elements mElementUtils;
+    protected Messager mMessager;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         mElementUtils = processingEnv.getElementUtils();
+        mMessager = processingEnv.getMessager();
+        Map<String, String> optionMap = processingEnv.getOptions();
+        Set<Map.Entry<String, String>> entrySet = optionMap.entrySet();
+        mMessager.printMessage(Diagnostic.Kind.NOTE, "---------------BaseAbstractProcessor init--------------------------");
+        for (Map.Entry<String, String> entry : entrySet) {
+            mMessager.printMessage(Diagnostic.Kind.NOTE, "BaseAbstractProcessor init:" + entry.getKey() + "-" + entry.getValue());
+        }
     }
 
     @Override
